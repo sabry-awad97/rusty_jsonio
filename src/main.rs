7f5a9451663ddd211f2fb691380177e0d1b281bd
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-02 21:05:57 +0200

    feat: Add support for reading JSON object from a string

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
-use serde_json::Value;
-use std::error::Error;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
-fn read_json_file(file_path: &str) -> Result<Vec<Value>, Box<dyn Error>> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
-    let mut json_vec = Vec::new();
-
-    for line in reader.lines() {
-        let json_string = line?;
-        let json_value = serde_json::from_str(&json_string)?;
-        json_vec.push(json_value);
-    }
-
-    Ok(json_vec)
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    address: String,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let json_values = read_json_file("example.json")?;
-    for value in json_values {
-        println!("{}", value);
-    }
+fn main() -> Result<()> {
+    let json_string = r#"
+        {
+            "name": "John Doe",
+            "age": 30,
+            "address": "123 Main St."
+        }
+    "#;
+
+    let person: Person = serde_json::from_str(json_string)?;
+
+    println!("Name: {}", person.name);
+    println!("Age: {}", person.age);
+    println!("Address: {}", person.address);
+
     Ok(())
 }
